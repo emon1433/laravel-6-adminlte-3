e4944e5a2c89b996554f05eee84d8edba6202c91
emon1433 <sazid1433@cseku.ac.bd>
2019-12-04 10:39:48 +0600

    adminlte 3 integreted

---
+++ resources/views/theme/partials/js.blade.php
@@ -0,0 +1,19 @@
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<!-- <script src="plugins/jquery/jquery.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+
+<!-- Bootstrap -->
+<!-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha256-fzFFyH01cBVPYzl16KT40wqjhgPtq6FFUB6ckN2+GGw=" crossorigin="anonymous"></script>
+
+<!-- overlayScrollbars -->
+<!-- <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@1.10.0/js/OverlayScrollbars.min.js"></script>
+
+
+
+<!-- AdminLTE App -->
+<!-- <script src="dist/js/adminlte.js"></script> -->
+<script src="{{ asset('dist/js/adminlte.js') }}"></script>
